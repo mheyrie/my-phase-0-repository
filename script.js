b80b0ca6b52d4d21cbb1ad679b9fe230db3ef6d4
Mary <omota4fidelity@gmail.com>
2023-08-07 23:55:36 +0100

    add script.js, connect to index.html

---
+++ script.js
@@ -0,0 +1,7 @@
+const p = document.createElement("p");
+p.textContent = "Article evident arrived express highest men did boy. Mistress sensible entirely am so. Quick can manor smart money hopes worth too. Comfort produce husband boy her had hearing. Law others theirs passed but wishes. You day real less till dear read. Considered use dispatched melancholy sympathize discretion led. Oh feel if up to till like. He an thing rapid these after going drawn or. Timed she his law the spoil round defer. In surprise concerns informed betrayed he learning is ye. Ignorant formerly so ye blessing. He as spoke avoid given downs money on we. Of properly carriage shutters ye as wandered up repeated moreover.";
+document.querySelector("section").appendChild(p);
+
+const h2 = document.createElement("h2");
+h2.textContent = "Get your free consultation now!";
+document.querySelector("body").appendChild(h2);
